f0e6c8aea2024477b230d0da694d6250950a1baf
Jessica Wicksnin <jwicksnin@gmail.com>
2017-10-21 16:59:21 -0700

    Create List component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Annie from './Annie_Sniffs.gif';
+import List from './List.js';
 import './App.css';
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <List/>
       </div>
     );
   }
---
+++ src/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './List.css';
+
+class List extends Component {
+  render() {
+    return (
+      <div className="List">
+        My List:
+      </div>
+    );
+  }
+}
+
+export default List;
